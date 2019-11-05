fe00f53349d5bdedddf5f4f8f09635444aa2430b
PaulM-Robertson <paulmartin.robertson@gmail.com>
2019-11-05 11:49:12 -0700

    added buttons

--- timer/src/containers/TimerControls/TimerControls.js
+++ timer/src/containers/TimerControls/TimerControls.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '../../components/UI/Button/Button';
+import classes from './TimerControls.module.css';
 
-import classes from './TimerControls.module.css'
 const timerControls = () => {
     return (
         <div className = { classes.TimerControls }>
-            <Button>Start</Button>
+            <Button>Start/Stop</Button>
+            <Button>Reset</Button>
+            <Button>Lap</Button>
+            <Button>Settings</Button>
         </div>
     )
 }
