7b19f769c2f4749f756d1c93f33ce41f37d12418
Colin Hale <halecolin1@gmail.com>
2019-11-05 12:34:42 -0700

    Changing TimerControls to a class

--- timer/src/containers/TimerControls/TimerControls.js
+++ timer/src/containers/TimerControls/TimerControls.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React , { Component } from 'react';
 import Button from '../../components/UI/Button/Button';
 import classes from './TimerControls.module.css';
 
-const timerControls = () => {
-    return (
-        <div className = { classes.TimerControls }>
-            <Button>Start</Button>
-            <Button>Stop</Button>
-            <Button>Reset</Button>
-            <Button>Lap</Button>
-            <Button>Settings</Button>
-        </div>
-    )
+class TimerControls extends Component {
+    render() {
+        return (
+            <div className = { classes.TimerControls }>
+                <Button>Start</Button>
+                <Button>Stop</Button>
+                <Button>Reset</Button>
+                <Button>Lap</Button>
+                <Button>Settings</Button>
+            </div>
+        )
+    }
 }
 
 export default timerControls;
